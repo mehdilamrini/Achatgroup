c010ae67329acb25ab7d40a40f492a174830dd96
mac <mac@MacBook-Pro.Home>
2017-12-16 02:01:08 +0000

    4 commit

---
+++ src/achatcollectif/metier/ISendMailMetier.java
@@ -0,0 +1,8 @@
+package achatcollectif.metier;
+
+public interface ISendMailMetier {
+	
+	
+	public void SendMail();
+
+}
---
+++ src/achatcollectif/presentation/actions/SendEmailAction.java
@@ -0,0 +1,18 @@
+package achatcollectif.presentation.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+
+
+
+public class SendEmailAction extends ActionSupport {
+	
+	
+	
+		
+		
+	}
+	
+	
+
+
